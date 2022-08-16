3304724e13dd5f82ac10658d20934535b6a9c8dc
Jonas Viana <jonasviana197@gmail.com>
2022-08-16 11:59:47 -0300

    adicionando pagina de post e começando a criar hook de criar post

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,75 @@
 import styles from './CreatePost.module.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuthValue } from '../../context/AuthContext'
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('')
+  const [image, setImage] = useState('')
+  const [body, setBody] = useState('')
+  const [tag, setTag] = useState([])
+  const [formError, setFormError] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
   return (
-    <div>
-      <h1>Criar post</h1>
+    <div className={styles.post}>
+      <h2>Criar Post</h2>
+      <p>Escreva sobre o que quiser e compartilhe seu conhecimento!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Titulo</span>
+          <input
+            type="text"
+            name="tittle"
+            required
+            placeholder="Titulo..."
+            onChange={e => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Url da imagem</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem"
+            onChange={e => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Conteúdo do post"
+            onChange={e => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags</span>
+          <input
+            type="text"
+            name="tag"
+            required
+            placeholder="Insira tags separados por virgula"
+            onChange={e => setTag(e.target.value)}
+            value={tag}
+          />
+        </label>
+        <button className="btn">Postar</button>
+        {/*!loading && <button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className={styles.error}>{error}</p>*/}
+      </form>
     </div>
   )
 }
